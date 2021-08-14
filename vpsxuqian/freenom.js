48c3e55e2934d0f3a53b8e50bd39964a3839c3e1
guilongjie <72298325+guilongjie@users.noreply.github.com>
2021-08-14 09:04:54 +0800

    Update freenom.js

--- vpsxuqian/freenom.js
+++ vpsxuqian/freenom.js
@@ -9,7 +9,7 @@ $exec('wget https://raw.githubusercontent.com/guilongjie/guigui/main/vpsxuqian/F
 })
 // 通知填写参考 https://raw.githubusercontent.com/whyour/qinglong/master/sample/config.sample.sh
 console.log("⏳ 开始执行 FNplus.py")
-$exec('python3 https://raw.githubusercontent.com/Oreomeow/freenom-py/main/FNplus.py', {
+$exec('python3 https://raw.githubusercontent.com/guilongjie/guigui/main/vpsxuqian/FNplus.py', {
   cwd: './script/Shell',
   timeout: 0,
   env: {
